941176503c7a5f419b4de0c44ae26b89677acad8
Ankit Agrawal <sainizameps@gmail.com>
2025-05-01 17:30:29 +0530

    setup custom loader

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+const App = ({ size = 40, color = "dodgerblue" }) => {
+  return (
+    <div
+      className="loading-spinner"
+      style={{
+        width: size,
+        height: size,
+        border: `4px solid ${color}`,
+        borderTop: "4px solid transparent",
+      }}
+    />
+  );
+};
+
+export default App;

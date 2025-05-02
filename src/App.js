fbf4568d2d565bb66cc96b9769632e0a9a55be99
Ankit Agrawal <sainizameps@gmail.com>
2025-05-02 12:03:36 +0530

    added isloading funtionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-const App = ({ size = 40, color = "dodgerblue" }) => {
+const App = ({ size = 40, color = "dodgerblue", loading = true }) => {
   return (
     <div
       className="loading-spinner"
@@ -10,6 +10,7 @@ const App = ({ size = 40, color = "dodgerblue" }) => {
         height: size,
         border: `4px solid ${color}`,
         borderTop: "4px solid transparent",
+        display: loading ? "flex" : "none",
       }}
     />
   );
